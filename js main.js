0433b61db8f733002882addddb8d4a27aebea44a
sabina547 <63221726+sabina547@users.noreply.github.com>
2020-04-08 14:35:01 +0545

    first class of js

---
+++ js main.js
@@ -0,0 +1,63 @@
+console.log('Hello World');
+console.error('thid is an error');
+console.warn('thid is a warning');
+
+
+
+//let,const
+
+const age=30;
+age = 31;
+console.log(age);
+
+let score;
+score = 10;
+console.log(score);
+
+
+// datatypes: Strings, numbers, Boolean,null,undefined
+
+const name ='Sabina';
+const age= 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+
+console.log(typeofy);
+
+//Concatenation
+console. log('My name is ' +name+'and I am ' +age);
+
+//Template String
+const hello ='MY name is ${name} and I am ${age}';
+
+console .log(hello);
+
+
+//Arrays: it hold multiple values
+
+const numbers= new Array(1,2,3,4,5);
+console. log(numbers);
+
+//anothers war to declare array
+
+const fruit = ['apples', 'oranges', 'pears'];
+
+fruits[3] = 'grapes';
+console .log(fruits[1]);
+
+//push method:
+fruits.push('mangos');
+
+//pop method:delete last one 
+fruits.pop();
+
+//to add begining we use unshift method
+fruits.unshift('papaya');
+
+
+
+
